b3211ec33af71c3abd4e3b83b266c55cf1f1f0fd
HeZhipeng <yearhe@outlook.com>
2018-12-04 18:13:54 +0800

    3

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Layout, Menu, Row, Col, Card } from 'antd';
+const { Header, Content } = Layout;
+function see({ item, key, selectedKeys }) {
+  console.log(item)
+  console.log(key)
+  console.log(selectedKeys)
+};
 
-class ACC extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+      <Layout>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }} onSelect={see}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ marginTop: 64 }}>
+          <div style={{ padding: '10px' }}>
+            <Row gutter={16}>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>Card content</Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>Card content</Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>Card content</Card>
+              </Col>
+            </Row>
+          </div>
+        </Content>
+      </Layout>
     );
   }
 }
 
-export default ACC;
+export default App;
